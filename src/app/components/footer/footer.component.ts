2a4a02d989ba5dca46e1a6e6b8e4ea84af063a77
SAK74 <sergijks@gmail.com>
2023-03-27 17:42:29 +0200

    feat: add footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'footer-component',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {}
--- src/app/interceptors/setSpinner-interceptor.ts
+++ src/app/interceptors/setSpinner-interceptor.ts
@@ -1,31 +1,19 @@
 import {
-  // HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { finalize, tap } from 'rxjs/operators';
-// import { SpinnerComponent } from '../components';
+import { finalize } from 'rxjs/operators';
 import { SpinnerService } from '../components/spinner/spinner.service';
 
 @Injectable({ providedIn: 'root' })
 export class SetSpinnerInterceptor implements HttpInterceptor {
   constructor(private spinner: SpinnerService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // console.log('spinner interceptor');
     this.spinner.start();
-    // console.log('spinner interceptor', req);
     return next.handle(req).pipe(
-      tap({
-        next: (ev) => {
-          // console.log('next: ', ev);
-          // this.spinner.stop();
-        },
-        // complete: () => console.log('complete'),
-      }),
       finalize(() => {
-        console.log('finalize');
         this.spinner.stop();
       })
     );
